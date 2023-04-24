eb01d6e2af134c43fc404da5a005da6cb5cf2daa
diptanshumittal <diptanshu@google.com>
2023-04-24 13:05:29 +0530

    Added releasetool publisher

--- internal/kokoro/release.sh
+++ internal/kokoro/release.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 # Fail on any error.
-set -e
+set -eo pipefail
+
+# Always run the cleanup script, regardless of the success of bouncing into
+# the container.
+function cleanup() {
+    chmod +x ${KOKORO_GFILE_DIR}/trampoline_cleanup.sh
+    ${KOKORO_GFILE_DIR}/trampoline_cleanup.sh
+    echo "cleanup";
+}
+trap cleanup EXIT
+
+$(dirname $0)/populate-secrets.sh # Secret Manager secrets.
+
+# Start the releasetool reporter
+python3 -m releasetool publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
 
 cd github/google-cloud-go/
 
@@ -9,27 +23,58 @@ cd github/google-cloud-go/
 mkdir pkg
 
 # Test prechecks
-if [[ -z "${AUTORELEASE_PR:-}" ]]; then
-  echo "AUTORELEASE_PR not set. Exiting"
+if [ -z "${AUTORELEASE_PR}" ]
+then
+  echo "Need to provide URL to release PR via AUTORELEASE_PR environment variable"
   exit 1
 fi
 
 # Extract the PR number from the AUTORELEASE_PR variable
-PR_NUMBER=$(echo $AUTORELEASE_PR | awk -v RS='/' 'END{print}')
+get_pr_number () {
+  echo $(echo $1 | awk -v RS='/' 'END{print}')
+}
 
-# Get the list of products released in the PR. 
-array=($(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" \
- https://api.github.com/repos/googleapis/google-cloud-go/pulls/$PR_NUMBER/files | \
- jq -r 'map(select(.filename == ".release-please-manifest-individual.json" or .filename == ".release-please-manifest-submodules.json") | .patch)[0]' | \
- awk -v RS='\n' '/^[+]/{print substr($2,2, length($2)-3)}'))
+# Get the PR number 
+pr_number=$(get_pr_number $AUTORELEASE_PR)
 
-echo ${array[@]}
-# For each products create a zip of released versions and store it in pkg 
+# Returns the list of modules released in the PR. 
+release_modules () {
+  pr_diff=$(curl https://api.github.com/repos/googleapis/google-cloud-go/pulls/$1/files)
+  echo $(jq -r 'map(select(.filename == ".release-please-manifest-individual.json" or .filename == ".release-please-manifest-submodules.json") | .patch)[0]' <<< $pr_diff | \
+         awk -v RS='\n' '/^[+]/{print substr($2,2, length($2)-3)}')
+}
+
+# Get the list of released modules
+release_modules_list=($(release_modules $pr_number))
+
+# For each module create a zip of released versions and store it in pkg 
 # directory to be picked up as an artifact by the kokoro job
-for element in "${array[@]}"
+for module in "${release_modules_list[@]}"
 do
-    zip -r "pkg/$element.zip" $element
+    zip -r "pkg/$module.zip" $module
 done
 
 # Store the commit hash in a txt as an artifact.
 echo -e $KOKORO_GITHUB_COMMIT >> pkg/commit.txt
+
+# Test!
+sample_pr_1="https://github.com/googleapis/google-cloud-go/pull/7687"
+sample_pr_2="https://github.com/googleapis/google-cloud-go/pull/7701"
+
+pr_number_1=$(get_pr_number $sample_pr_1)
+pr_number_2=$(get_pr_number $sample_pr_2)
+
+if [ "$pr_number_1" != "7687" ] ; then
+  echo "Error: Incorrect value from get_pr_number."
+  exit
+fi
+
+if [ "$(release_modules $pr_number_1)" != "aiplatform appengine compute contactcenterinsights container iap retail security workstations" ] ; then
+  echo "Error: Incorrect value from release_modules."
+  exit
+fi
+
+if [ "$(release_modules $pr_number_2)" != "bigquery" ] ; then
+  echo "Error: Incorrect value from release_modules."
+  exit
+fi
